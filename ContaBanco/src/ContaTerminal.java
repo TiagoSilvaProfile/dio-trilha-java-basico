6022e3e0bb6147adf28bce28ef2bc916bbc49ae3
Tiago.Antonio <tiago.antonio@mv.com.br>
2024-06-05 13:32:31 -0300

    [DIO-02] Meu projeto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Exibir as mensagens para o nosso usuário e obter os valores digitados no terminal
+        System.out.println("Por favor, digite o seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da sua conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da sua agência:");
+        scanner.nextLine(); // Consumir a quebra de linha pendente
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        // Exibir a mensagem conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        // Fechar o scanner
+        scanner.close();
     }
 }
